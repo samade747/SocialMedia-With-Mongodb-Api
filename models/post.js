ca9bc64cff4e031d471df21b53893a59e96d7380
samade747 <samad.e747@gmail.com>
2024-02-01 12:02:13 +0500

    postSchema

---
+++ models/post.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId:{
+        type: String,
+        required: true,
+    },
+    desc:{
+        type: String,
+        max: 200,
+    },
+    image:{
+        type:String,
+    },
+    likes:{
+        type: Array,
+        default: [],
+    },
+}, 
+{ timestamps: true}
+
+);
